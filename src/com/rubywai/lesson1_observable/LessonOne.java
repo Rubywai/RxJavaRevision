7ac66dc28873d4168c6bbe85b07cb237b38b2fa6
Ruby Learner <rubyland.myanmar.mg@gmail.com>
2021-01-04 16:26:19 +0630

    observable

---
+++ src/com/rubywai/lesson1_observable/LessonOne.java
@@ -0,0 +1,40 @@
+package com.rubywai.lesson1_observable;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LessonOne {
+    public static void main(String[] args) {
+       // observableWithJust();
+       // observableWithInterable();
+        observableWithCreate();
+    }
+
+    static void observableWithJust(){
+        Observable<String> observable = Observable.just("12","13","hello");
+        observable.subscribe(System.out::println);
+
+    }
+    static void observableWithInterable(){
+        List<String> list = Arrays.asList("one","two","three","four");
+        Observable<String> observable = Observable.fromIterable(list);
+
+        observable.subscribe(System.out::println);
+    }
+    private static void observableWithCreate(){
+        Observable<String> observable = Observable.create(emitter -> {
+            emitter.onNext("one");
+            Thread.sleep(1000);
+            emitter.onNext("two");
+            Thread.sleep(1000);
+            emitter.onNext("three");
+            Thread.sleep(1000);
+            emitter.onNext("four");
+            Thread.sleep(1000);
+            emitter.onComplete();
+        });
+        observable.subscribe(System.out::println);
+    }
+}
